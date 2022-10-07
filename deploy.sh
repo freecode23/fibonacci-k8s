e716d82576639e51014deb141c056dbd113ed8eb
sherly <sharton1@asu.edu>
2022-10-07 13:11:34 +0700

    change deploy sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,17 @@
-docker build -t cygnetops/multi-client-k8s:latest -t cygnetops/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
-docker build -t cygnetops/multi-server-k8s-pgfix:latest -t cygnetops/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
-docker build -t cygnetops/multi-worker-k8s:latest -t cygnetops/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+# deployment
+docker build -t freecode23/fibonacci-client-k8s -t freecode23/fibonacci-client-k8s -f ./client/Dockerfile ./client
+docker build -t freecode23/fibonacci-server-k8s -t freecode23/fibonacci-server-k8s -f ./server/Dockerfile ./server
+docker build -t freecode23/fibonacci-worker-k8s -t freecode23/fibonacci-worker-k8s -f ./worker/Dockerfile ./worker
 
-docker push cygnetops/multi-client-k8s:latest
-docker push cygnetops/multi-server-k8s-pgfix:latest
-docker push cygnetops/multi-worker-k8s:latest
+docker push freecode23/fibonacci-client-k8s
+docker push freecode23/fibonacci-server-k8s
+docker push freecode23/fibonacci-worker-k8s
 
-docker push cygnetops/multi-client-k8s:$SHA
-docker push cygnetops/multi-server-k8s-pgfix:$SHA
-docker push cygnetops/multi-worker-k8s:$SHA
+docker push freecode23/fibonacci-client-k8s
+docker push freecode23/fibonacci-server-k8s
+docker push freecode23/fibonacci-worker-k8s
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=cygnetops/multi-server-k8s-pgfix:$SHA
-kubectl set image deployments/client-deployment client=cygnetops/multi-client-k8s:$SHA
-kubectl set image deployments/worker-deployment worker=cygnetops/multi-worker-k8s:$SHA
+kubectl set image deployments/server-deployment server=freecode23/fibonacci-server-k8s
+kubectl set image deployments/client-deployment client=freecode23/fibonacci-client-k8s
+kubectl set image deployments/worker-deployment worker=freecode23/fibonacci-worker-k8s
